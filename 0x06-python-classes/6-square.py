66e8fc8f4829d54b0593fc4b6afcca1c586c57f3
NaaPhyl <pnlotdh@gmail.com>
2023-01-12 13:34:01 +0000

    task done

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """Define a class Square."""
 
 
@@ -47,7 +48,7 @@ class Square:
 
     def my_print(self):
         """Print the square with the # character."""
-        if self.__size == 0:
+       if self.__size == 0:
             print("")
             return
 
@@ -55,4 +56,4 @@ class Square:
         for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
-          print("")
+            print("")
