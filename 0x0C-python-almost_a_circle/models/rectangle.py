59957fe69bc7b2f9432fc60fea086a1a989f15f3
NaaPhyl <pnlotdh@gmail.com>
2023-01-23 10:44:24 +0000

    1-write_file.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,72 +1,38 @@
 #!/usr/bin/python3
-
 """Defines a rectangle module (modules.rectangle)"""
-
 from models.base import Base
 
 
-
-
-
 class Rectangle(Base):
-
     """Defines a rectangle class"""
 
-
-
     def __init__(self, width, height, x=0, y=0, id=None):
-
         """Method that init values for a rectangle object
-
         Args:
-
            width:size of the width
-
            height: size of the height
-
            x: Variable x
-
            y:  Variable y
-
         Return:
-
            Always nothing
-
         """
-
         self.width = width
-
         self.height = height
-
         self.x = x
-
         self.y = y
-
         super().__init__(id)
 
-
-
-    # Getter and setter of width
-
+   # Getter and setter of width
     @property
-
     def width(self):
-
         """Getter the size of width
-
         """
-
         return self.__width
 
-
-
     @width.setter
-
     def width(self, value):
-
         """Setter the size of width
-
-        Args:
+       Args:
 
            value: Size to assign to the width
 
